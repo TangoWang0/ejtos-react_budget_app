42d2e574441b2321ecd15f7d8afc00a0992efe88
Ricky Santini <rickysantini3@gmail.com>
2023-10-03 12:46:41 -0400

    finally

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -5,7 +5,7 @@ import { AppContext } from '../context/AppContext';
 const AllocationForm = (props) => {
     const { dispatch,remaining, currency } = useContext(AppContext);
 
-    const {dispatch,remaining} = useContext(AppContext)
+    const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
 
